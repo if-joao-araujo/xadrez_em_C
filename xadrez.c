d098afbc23c24519344b4765523d551c191ff1f6
João pedro santana de araújo <if.joao.araujo@gmail.com>
2025-04-06 12:18:18 -0300

    Update xadrez.c
    Simula os movimentos das peças do xadrez(com exceção do pião), um exercício interessante para eu treinar minha lógica.
    Tem melhorias para fazer, quem sabe depois eu não acrescente funcionalidades adicionais( ou deixar o jogo completo).

--- xadrez.c
+++ xadrez.c
@@ -1,35 +1,100 @@
-
 #include <stdio.h>
 
-int main() {
-    
-    int i = 0; 
-    int movimento_peça;
-    
-    char escolha_torre;
-    char ataque_torre[6] = "cima"; 
-    char recuo_torre[6] = "baixo"; 
-    
-    printf("Deseja movimentar qual peça:\n [1] Torre\n [2] Bispo\n [3] Torre\n [4] Cavalo\n:");
-    scanf("%d", &movimento_peça);
-    
-    if (movimento_peça == 1) {
-        printf("Você escolheu torre\n");
-        printf("Deseja atacar [a] ou recuar [r]: ");
-        scanf(" %c", &escolha_torre);  
+void moverTorre(int casas, int acao) {
+    if (casas <= 0) return;
+    printf(acao == 1 ? "Direita\n" : "Esquerda\n");
+    moverTorre(casas - 1, acao);
+}
 
-        if (escolha_torre == 'a') { 
-            printf("Movimento de ataque: ");
-            for(i;5;i++ ){
-                printf("%c", ataque_torre); 
-                i++;
-            }else if (escolha_torre == 'r'){
-               for(i;5; i++){
-                  printf("%c", recuo_torre);
+void moverBispo(int casas, int acao) {
+    if (casas <= 0) return;
+
+    for (int i = 0; i < 1; i++) {  
+        for (int j = 0; j < 1; j++) {  
+            if (acao == 1)
+                printf("Cima, Direita\n");
+            else
+                printf("Baixo, Esquerda\n");
+        }
+    }
+
+    moverBispo(casas - 1, acao);
+}
+
+void moverRainha(int casas, int acao) {
+    if (casas <= 0) return;
+    printf(acao == 1 ? "Esquerda\n" : "Direita\n");
+    moverRainha(casas - 1, acao);
+}
+
+void moverCavalo(int acao) {
+    int cima = 2;      
+    int direita = 1;    
+
+    for (int i = 1; i <= cima + 1; i++) {
+        for (int j = 1; j <= direita + 1; j++) {
+            if (i > cima && j > direita) {
+                break;  
+            }
+
+            if (acao == 1) {
+                if (i <= cima) {
+                    printf("Cima\n");
+                    if (i == cima && j == 1) continue; 
+                }
+                if (j <= direita && i > cima - 1) {
+                    printf("Direita\n");
+                }
+            } else {
+                if (i <= cima) {
+                    printf("Baixo\n");
+                    if (i == cima && j == 1) continue; 
+                }
+                if (j <= direita && i > cima - 1) {
+                    printf("Esquerda\n");
                 }
             }
         }
-        
     }
+}
+
+int main() {
+    int escolhaPeca, escolhaAcao;
+
+    printf("Escolha uma peça:\n");
+    printf("[1] Torre\n[2] Bispo\n[3] Cavalo\n[4] Dama\n");
+    scanf("%d", &escolhaPeca);
+
+    printf("Escolha uma ação:\n");
+    printf("[1] Atacar\n[2] Recuar\n");
+    scanf("%d", &escolhaAcao);
+
+    switch (escolhaPeca) {
+        case 1:
+            moverTorre(5, escolhaAcao);
+            break;
+        case 2:
+            moverBispo(5, escolhaAcao);
+            break;
+        case 3:
+            moverCavalo(escolhaAcao);
+            break;
+        case 4:
+            moverRainha(8, escolhaAcao);
+            break;
+        default:
+            printf("Opção inválida.\n");
+            return 1;
+    }
+
+    // Mensagem final
+    if (escolhaAcao == 1) {
+        printf("Você avançou.\n");
+    } else if (escolhaAcao == 2) {
+        printf("Você recuou.\n");
+    } else {
+        printf("Ação inválida.\n");
+    }
+
     return 0;
 }
